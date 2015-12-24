29b31695c731be60737130b96a0866b2de96af5f
Simon Marchi <simon.marchi@polymtl.ca>
2015-12-23 22:16:00 -0500

    Add missing common-config.h

---
+++ common/common-config.h
@@ -0,0 +1,21 @@
+#ifndef COMMON_CONFIG_H
+#define COMMON_CONFIG_H
+
+#define MAX_CUBES 6
+
+struct master_to_slave_data {
+  uint8_t rank;
+  uint8_t word[MAX_CUBES];
+};
+
+struct slave_to_master_data {
+  uint8_t cubes[MAX_CUBES];
+};
+
+typedef union {
+  struct master_to_slave_data m2s;
+  struct slave_to_master_data s2m;
+} comm_buf;
+
+#endif /* COMMON_CONFIG_H */
+
